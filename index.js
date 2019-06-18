ecaaa8c7c51297b7f8492e8b5e5470e72c80fb49
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-06-18 03:07:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ var funkyFunction = function() {
     return "FUNKY!"
   }
   }
-  }
   
 // We want to set theFunk equal to "Funky!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
